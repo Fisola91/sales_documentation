52220c1a74b3c8ca09ab64ccbb61bad0803ef5ef
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-10-04 07:53:58 +0200

    Implemented grand total quantity for all orders

--- app/components/summary_table.rb
+++ app/components/summary_table.rb
@@ -11,6 +11,13 @@ class SummaryTable < ViewComponent::Base
     total_sum
   end
 
+  def grand_total_quantity
+    total_quantity = orders.reduce(0) do |sum, hash| 
+      sum + hash[:quantity] 
+    end
+    total_quantity
+  end
+
   attr_reader :orders, :show
 
   def show_class
