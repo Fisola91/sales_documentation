6e4f2f7032610889cf25796af0edcbc18983a52d
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-10-23 20:35:35 +0200

    Set up the state required for all examples in the before each block

--- spec/system/sales_per_day_spec.rb
+++ spec/system/sales_per_day_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe "Sales per day", type: :system do
   before do
     visit '/'
+    expect(page).to have_link("all sales")
+    click_link "all sales"
   end
 
   fit "displays the form page and link to all sales page" do
-    expect(page).to have_link("all sales")
-
-    click_link "all sales"
-
     expect(page).to have_content("ALL SALES PER DAY")
   end
 end
