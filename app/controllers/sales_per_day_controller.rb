45db9bd27f8cb267119e25e968f3bb2dd61cad08
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-10-23 20:21:56 +0200

    Created sales_per_day files for controller, view(index) and spec

---
+++ app/controllers/sales_per_day_controller.rb
@@ -0,0 +1,2 @@
+class SalesPerDayController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "prototype", to: "public#index"
 
   resources :orders, except: %i(show)
+  resources :sales_per_day, only: %i(index)
 
   root "orders#index"
 end
