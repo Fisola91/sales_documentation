fca9bf3ff358377e55317e7f9e30c64f49273af0
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-10-23 20:23:14 +0200

    Test: Form is dispayed on the root page and have the link to all sales_per_day page

---
+++ spec/system/sales_per_day_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Sales per day", type: :system do
+  before do
+    visit '/'
+  end
+
+  fit "displays the form page and link to all sales page" do
+    expect(page).to have_link("all sales")
+
+    click_link "all sales"
+
+    expect(page).to have_content("ALL SALES PER DAY")
+  end
+end
