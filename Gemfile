ed9bce96239e43d00a19cbb30d2e2450ae5122b5
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-10-24 13:58:00 +0200

    Removed faker gem and changed its values to explicit values

--- Gemfile
+++ Gemfile
@@ -22,7 +22,6 @@ group :development, :test do
   gem "guard-rspec", require: false
   gem "pry"
   gem "rspec-rails", "~> 6.0.3"
-  gem 'faker'
 end
 
 group :development do
--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
     factory :order do
-      name { Faker::Commerce.product_name }
-      quantity { Faker::Number.decimal(l_digits: 2) }
-      unit_price { Faker::Number.decimal(l_digits: 2) }
+      sequence(:name) { |n| "Product #{n}" }
+      quantity { 10.0 }
+      unit_price { 2.0 }
       total { quantity.to_f * unit_price.to_f }
     end
   end
