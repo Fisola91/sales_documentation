e927c6235769d9ea0fa33e62e106b827aaf36813
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-11-08 01:21:15 +0100

    Extracted order attributes (quantity, unit_price, total) into different methods

--- app/models/order_manager.rb
+++ app/models/order_manager.rb
@@ -4,29 +4,48 @@ class OrderManager
   end
 
   def create_order
-    quantity = safe_params.fetch(:quantity).to_f
-    unit_price = safe_params.fetch(:unit_price).to_f
+    quantity
+    unit_price
 
     created_date = params["date"]
-    total = quantity * unit_price
     order_params = safe_params.merge(total: total, created_at: created_date)
 
     order = Order.new(order_params)
   end
 
   def update_order
-    quantity = params.dig(:order, :quantity).to_f
-    unit_price = params.dig(:order, :unit_price).to_f
+    quantity
+    unit_price
 
-    total = quantity * unit_price
     order_params = safe_params.merge(total: total)
   end
 
 
   private
+
   attr_reader :params
 
   def safe_params
     params.require(:order).permit(:name, :quantity, :unit_price)
+  end
+
+  def quantity
+    if safe_params
+      safe_params.fetch(:quantity).to_f
+    else
+      params.dig(:order, :quantity).to_f
+    end
+  end
+
+  def unit_price
+    if safe_params
+      safe_params.fetch(:unit_price).to_f
+    else
+      params.dig(:order, :unit_price).to_f
+    end
+  end
+
+  def total
+    quantity * unit_price
   end
 end
