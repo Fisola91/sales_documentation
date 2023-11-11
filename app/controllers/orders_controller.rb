1336a947b4b11e7fe28499bd80d8c509968ae726
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-11-12 00:48:45 +0100

    Changed OrderManager class argument from params to explicit values needed for the class to work

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    date = Date.today
+    today_date = Date.today
     @form = OrderForm.new
-    if params["date"]
-      orders = all_orders(params["date"])
+    if date_from_params
+      orders = all_orders(date_from_params)
       @form = OrderForm.new(date: orders.first.created_at)
       @summary = SummaryTable.new(orders: orders)
     else
-      @summary = SummaryTable.new(orders: all_orders(date))
+      @summary = SummaryTable.new(orders: all_orders(today_date))
     end
   end
 
   def create
-    order = OrderManager.new(params).create_order
+    order = OrderManager.new(
+      safe_params,
+      quantity,
+      unit_price,
+      date: date_from_params
+    ).create_order
+
 
     if order.save
       respond_to do |format|
@@ -38,12 +44,16 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @form = OrderForm.new(order: @order)
 
-    @summary = SummaryTable.new(orders: all_orders(@order.created_at))
+    @summary = SummaryTable.new(orders: all_orders(date_order_created(@order)))
   end
 
   def update
     order = Order.find(params[:id])
-    order_params = OrderManager.new(params).update_order
+    order_params = OrderManager.new(
+      safe_params,
+      quantity,
+      unit_price
+    ).update_order
 
     if order.update(order_params)
       redirect_to orders_url
@@ -59,13 +69,33 @@ class OrdersController < ApplicationController
       format.turbo_stream do
         render turbo_stream: turbo_stream.replace(
           "summary-table",
-          SummaryTable.new(orders: all_orders(order.created_at))
+          SummaryTable.new(orders: all_orders(date_order_created(order)))
         )
       end
     end
   end
 
   private
+
+  def safe_params
+    params.require(:order).permit(:name, :quantity, :unit_price)
+  end
+
+  def quantity
+    safe_params.fetch(:quantity).to_f
+  end
+
+  def unit_price
+    safe_params.fetch(:unit_price).to_f
+  end
+
+  def date_from_params
+    params["date"]
+  end
+
+  def date_order_created(order)
+    order.created_at
+  end
 
   def all_orders(date)
     @all_orders ||= Order.created_on(date).order(created_at: :asc)
--- app/models/order_manager.rb
+++ app/models/order_manager.rb
@@ -1,43 +1,26 @@
 class OrderManager
-  def initialize(params)
-    @params = params
+  def initialize(safe_params, quantity, unit_price, date: nil)
+    @safe_params = safe_params
+    @quantity = quantity
+    @unit_price = unit_price
+    @date = date
   end
 
   def create_order
-    created_date = params["date"]
+    created_date = date
     order_params = safe_params.merge(total: total, created_at: created_date)
 
-    order = Order.new(order_params)
+    Order.new(order_params)
   end
 
   def update_order
-    order_params = safe_params.merge(total: total)
+    safe_params.merge(total: total)
   end
 
 
   private
 
-  attr_reader :params
-
-  def safe_params
-    params.require(:order).permit(:name, :quantity, :unit_price)
-  end
-
-  def quantity
-    if safe_params
-      safe_params.fetch(:quantity).to_f
-    else
-      params.dig(:order, :quantity).to_f
-    end
-  end
-
-  def unit_price
-    if safe_params
-      safe_params.fetch(:unit_price).to_f
-    else
-      params.dig(:order, :unit_price).to_f
-    end
-  end
+  attr_reader :safe_params, :quantity, :unit_price, :date
 
   def total
     quantity * unit_price
