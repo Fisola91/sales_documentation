2c9d0a508181a674832feef592a85e1a64e913ad
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-12-26 16:25:08 +0100

    Test passed: changed registration/session view template to slim and added a new attribute to user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { "miga@gmail.com" }
+    username { "John" }
+    email { "john@gmail.com" }
     password { "123456" }
-    password_confirmation { "123456" }
   end
 end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -13,13 +13,14 @@ RSpec.describe "User", type: :system do
 
     expect(page).to have_link("Register")
 
-    fill_in "Email", with: "miga@gmail.com"
-    fill_in "Password", with: "123456"
+    fill_in "Username", with: "Suyi"
+    fill_in "Email", with: "suyi@gmail.com"
+    fill_in "user_password", with: "123456"
     fill_in "Password confirmation", with: "123456"
 
     click_on "Sign up"
 
-    expect(page).to have_content("Welcome, miga@gmail.com")
+    expect(page).to have_content("Welcome, Suyi")
     expect(page).to have_link("Sign out")
   end
 
@@ -34,12 +35,12 @@ RSpec.describe "User", type: :system do
     expect(page).to have_link("Sign up")
     expect(page).to have_link("Forgot your password?")
 
-    fill_in "Email", with: user.email
+    fill_in "Username", with: user.username
     fill_in "Password", with: user.password
 
     click_on "Log in"
 
-    expect(page).to have_content("Welcome, #{user.email}")
+    expect(page).to have_content("Welcome, #{user.username}")
     expect(page).to have_link("Sign out")
   end
 
@@ -54,12 +55,12 @@ RSpec.describe "User", type: :system do
     expect(page).to have_link("Sign up")
     expect(page).to have_link("Forgot your password?")
 
-    fill_in "Email", with: user.email
+    fill_in "Username", with: user.username
     fill_in "Password", with: user.password
 
     click_on "Log in"
 
-    expect(page).to have_content("Welcome, #{user.email}")
+    expect(page).to have_content("Welcome, #{user.username}")
 
     click_on "Sign out"
 
