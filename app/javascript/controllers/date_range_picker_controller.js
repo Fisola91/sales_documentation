57b8673ec22bbe745a9ddd947906fa6b84108d36
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-12-30 06:57:52 +0100

    Added other options to the date range picker

--- app/javascript/controllers/date_range_picker_controller.js
+++ app/javascript/controllers/date_range_picker_controller.js
@@ -4,6 +4,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     new DateRangePicker(this.element, {
+      alwaysShowCalendars: true,
+      autoApply: true,
+      showWeekNumbers: true,
+      ranges: {
+        'Today': [moment().startOf('day'), moment().endOf('day')],
+        'Yesterday': [moment().subtract(1, 'days').startOf('day'), moment().subtract(1, 'days').endOf('day')],
+        'Last 7 Days': [moment().subtract(6, 'days').startOf('day'), moment().endOf('day')],
+        'This Month': [moment().startOf('month').startOf('day'), moment().endOf('month').endOf('day')],
+      },
       locale: {
         format: "YYYY-MM-DD",
       },
