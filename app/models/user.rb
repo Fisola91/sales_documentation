24080a1bd1c4bc1f82dcae9993e8bcd7b140a15d
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-01-16 14:44:36 +0100

    Created association between user and order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :orders
   validates :email, :encrypted_password, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
