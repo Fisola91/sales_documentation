1b9878d02a085a33ec84d80cd4ca85483db3fe08
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-01-25 15:07:36 +0100

    Changed method name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,12 @@ module ApplicationHelper
     link_to "Enter sales", orders_path, class: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:shadow-outline"
   end
 
-  def all_sales_link
-    link_to "all sales", sales_per_day_index_path, class: "hover:bg-blue-200", style: "color: blue; text-decoration: underline;"
+  def hover_color
+    "hover:text-blue-950"
+  end
+
+  def color_style
+    "color: blue; text-decoration: underline;"
   end
 
   def all_sales_link_with_bg_blue
