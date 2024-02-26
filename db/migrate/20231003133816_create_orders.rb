ec7f7a7c105fc390887448ca0196da7b941ba034
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-02-26 14:34:29 +0100

    Seed the user and order record and establish a reference between the two models

--- db/migrate/20231003133816_create_orders.rb
+++ db/migrate/20231003133816_create_orders.rb
@@ -5,6 +5,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
       t.float :quantity, null: false
       t.float :unit_price, null: false
       t.float :total, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
