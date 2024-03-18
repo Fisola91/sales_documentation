8a26eb49fc9a823c8ce9647f4b4dffe7b544b668
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-03-18 11:56:18 +0100

    Moved the date format method to the ApplicationHelper module

--- app/components/sales_per_day_component.rb
+++ app/components/sales_per_day_component.rb
@@ -1,14 +1,11 @@
 class SalesPerDayComponent < ViewComponent::Base
+  include ApplicationHelper
   def initialize(orders:)
     @orders = orders
   end
 
   def ground_total
     orders.sum { |order| order[:total]}
-  end
-
-  def date_format(date)
-    date.strftime("%Y-%m-%d")
   end
 
   attr_reader :orders
